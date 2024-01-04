43a41f787cb30f88b42338dde03d3589aebd6681
박은빈 <corwoddl@naver.com>
2024-01-04 16:34:07 +0900

    - script생성

---
+++ m_Portfolio/Assets/Project/Scripts/Character/Player/PlayerDefinitions.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//! Player 캐릭터의 모든 것
+
+public class PlayerComponents
+{
+    //* 플레이어 캐릭터관련 컴포넌트
+}
+
--- m_Portfolio/Assets/Project/Scripts/Character/Player/PlayerMovement.cs
+++ m_Portfolio/Assets/Project/Scripts/Character/Player/PlayerMovement.cs
@@ -2,23 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    // Start is called before the first frame update
     void Start()
     {
 
     }
 
+    // Update is called once per frame
     void Update()
     {
 
     }
-
-    void FixedUpdate()
-    {
-
-    }
-
-
-
 }
