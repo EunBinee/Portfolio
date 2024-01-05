bacc87b0f24eee766294474c7a37b4f112200758
박은빈 <corwoddl@naver.com>
2024-01-05 16:02:30 +0900

    - 테스트 씬 구성

--- m_Portfolio/Assets/Project/Scripts/Character/Player/PlayerDefinitions.cs
+++ m_Portfolio/Assets/Project/Scripts/Character/Player/PlayerDefinitions.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 using System;
 
 //! Player 캐릭터의 모든 것
-
+[Serializable]
 public class PlayerComponents
 {
     //* 플레이어 캐릭터관련 컴포넌트
+    public Rigidbody rigid;
+    public Animator anim;
 }
 
