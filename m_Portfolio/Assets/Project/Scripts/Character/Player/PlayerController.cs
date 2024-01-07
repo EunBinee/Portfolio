afd897acc12561e9cf027076ddc9d9d2fe342ba4
박은빈 <corwoddl@naver.com>
2024-01-07 12:59:44 +0900

    - 임시

--- m_Portfolio/Assets/Project/Scripts/Character/Player/PlayerController.cs
+++ m_Portfolio/Assets/Project/Scripts/Character/Player/PlayerController.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    public PlayerMovement playerMovement;
+    public PlayerComponents playerComponents = new PlayerComponents();
+    public PlayerInput playerInput = new PlayerInput();
+    public PlayerOption playerOption = new PlayerOption();
+    public PlayerCurState playerCurState = new PlayerCurState();
+    public PlayerCurValue playerCurValue = new PlayerCurValue();
+
+    private PlayerComponents P_com => playerComponents;
+    private PlayerInput P_input => playerInput;
+    private PlayerOption P_option => playerOption;
+    private PlayerCurState P_State => playerCurState;
+    private PlayerCurValue P_Value => playerCurValue;
+
+
+
     void Start()
     {
-
+        playerMovement = GetComponent<PlayerMovement>();
+        playerMovement.PlayerMovement_Init();
     }
 
     void Update()
--- m_Portfolio/Assets/Project/Scripts/Character/Player/PlayerDefinitions.cs
+++ m_Portfolio/Assets/Project/Scripts/Character/Player/PlayerDefinitions.cs
@@ -7,8 +7,45 @@ using System;
 [Serializable]
 public class PlayerComponents
 {
-    //* 플레이어 캐릭터관련 컴포넌트
+    //* 플레이어 컴포넌트
     public Rigidbody rigid;
     public Animator anim;
+    public CapsuleCollider capsuleCollider;
+}
+[Serializable]
+public class PlayerInput
+{
+    //* 플레이어 Input
+    public float verticalMovement;   //상하
+    public float horizontalMovement; //좌우
+    public float mouseY;             //마우스 상하
+    public float mouseX;             //마우스 좌우
+    public float jumpMovement;       //점프
+}
+[Serializable]
+public class PlayerOption
+{
+    //* 플레이어 옵션 값
+
 }
 
+[Serializable]
+public class PlayerCurState
+{
+    //* 플레이어의 현재 상태
+    public bool isWalking;
+    public bool isRunning;
+    public bool isSprinting;
+    public bool isJumping;
+    public bool isStrafing;         //주목중
+    public bool isDodge;            //피하기 (ex. 구르기)
+    public bool isGround;           //현재 발이 바닥에 닿아있는지
+    public bool isForwardBlocked;   //앞에 장애물이 있는지 여부
+    public bool isOnSteepSlop;      //앞에 가파른 경사있음.
+}
+
+[Serializable]
+public class PlayerCurValue
+{
+    public float moveAmount = 0;     // 움직임. (0 움직이지않음, 1 움직임)
+}
